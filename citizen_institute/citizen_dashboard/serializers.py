86a4da721b2da833182a8830016c8f2cfd18f5d8
yosrzohair <yosirdirani@gmail.com>
2025-02-18 15:27:38 +0300

    Revert "Merge branch 'main' of github.com:kareem-hijjawi/blood_system"
    This reverts commit c0e61fd0b99d9daaa61c253d39655acb9994bb83, reversing
    changes made to d5ad883f2e853a6b4a16e0feb4176f68f8e7ba4c.

--- citizen_institute/citizen_dashboard/serializers.py
+++ citizen_institute/citizen_dashboard/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
-from .models import KhaldaHospitalAppointment,BloodDonationAppointment
+from .models import KhaldaHospitalAppointment
+from institute_dashboard.models import UrgentCaseList
 
 class KhaldaHospitalAppointmentSerializer(serializers.ModelSerializer):
     class Meta:
@@ -7,22 +8,7 @@ class KhaldaHospitalAppointmentSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 
-
-class BloodDonationAppointmentSerializer(serializers.ModelSerializer):
+class UrgentCaseListSerializer(serializers.ModelSerializer):
     class Meta:
-        model= BloodDonationAppointment
-        fields= '__all__'
-        
-    def validate(self, data):
-        """
-        Custom validation to ensure:
-        - Users who donated in the last 2 months can't donate now.
-        - Donation units can only be selected if donation is allowed.
-        """
-        if data.get('donated_last_two_months'):
-            raise serializers.ValidationError("You cannot donate now. Please wait at least 2 months.")
-        
-        if not data.get('donated_last_two_months') and not data.get('donation_units'):
-            raise serializers.ValidationError("Please select the number of donation units.")
-
-        return data
+        model = UrgentCaseList
+        fields = "__all__"
